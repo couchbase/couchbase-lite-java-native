39f1a90eb051f9479f3530fd1344d10a5183c0b1
hideki <hideki@couchbase.com>
2016-06-02 13:24:44 -0700

    Fixed https://github.com/couchbase/couchbase-lite-android/issues/801 - CouchBase crashes at Android N preview
    - deleted default-sqlite project files

--- sqlite-default/src/main/java/com/couchbase/lite/java/sqlitesystem/Native.java
+++
@@ -1,5 +0,0 @@
-package com.couchbase.lite.java.sqlitesystem;
-
-public class Native {
-  // This empty class allows the packaged jar file to be loaded into the classpath.
-}
--- sqlite-default/src/main/jni/Application.mk
+++
@@ -1,3 +0,0 @@
-# armeabi armeabi-v7a arm64-v8a x86 x86_64 mips mips64
-APP_ABI := all
-APP_STL := gnustl_static
